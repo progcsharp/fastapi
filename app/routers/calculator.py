b881893a49a0834060bff5fd60b0e17a612ee525
fonror <fonror@mail.ru>
2023-07-16 03:47:03 +0300

    fix calculator date

--- app/routers/calculator.py
+++ app/routers/calculator.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, HTTPException
 
 from datetime import date as d
 
@@ -9,6 +9,9 @@ router_calculator = APIRouter(prefix="/calculator", responses={404: {"descriptio
 
 
 @router_calculator.get("/")
-async def calculator(price: float, date: d, cargo_type: str):
-    rate = await get_rate(date, cargo_type.lower())
-    return {"price": price*rate.rate}
+async def calculator(price: float, cargo_type: str, date: d = d.today()):
+    try:
+        rate = await get_rate(date, cargo_type.lower())
+        return {"price": price*rate.rate}
+    except:
+        raise HTTPException(status_code=404, detail="Rate not found")
